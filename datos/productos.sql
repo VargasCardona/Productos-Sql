3a9944a168d2ef9e71cf2c7254fcdff264a3b2fe
Mateo Loaiza <141420478+Matthub05@users.noreply.github.com>
2023-08-21 16:28:59 -0500

    Actualizar base de datos

--- datos/productos.sql
+++ datos/productos.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Aug 18, 2023 at 03:12 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 21-08-2023 a las 23:27:35
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,29 +18,37 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `productos`
+-- Base de datos: `productos`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `productos`
+-- Estructura de tabla para la tabla `productos`
 --
 
 CREATE TABLE `productos` (
-  `nombre` varchar(250) NOT NULL,
   `sku` varchar(20) NOT NULL,
+  `nombre` varchar(250) NOT NULL,
+  `precio` double(15,2) NOT NULL,
   `distribuidor` varchar(250) NOT NULL,
-  `categoria` varchar(250) NOT NULL,
-  `precio` double(15,2) NOT NULL
+  `categoria` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Indexes for dumped tables
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`sku`, `nombre`, `precio`, `distribuidor`, `categoria`) VALUES
+('a001', 'Paracetamol', 12000.00, 'Amazon', 'Salud'),
+('a002', 'Acetaminofen', 11000.00, 'Google', 'Salud');
+
+--
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `productos`
+-- Indices de la tabla `productos`
 --
 ALTER TABLE `productos`
   ADD PRIMARY KEY (`sku`);
